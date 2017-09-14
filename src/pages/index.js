76bb6c750c4a15588c8172e50644b963ff465e06
p3732 <p3732@users.noreply.github.com>
2017-09-14 10:09:16 +0200

    added page for / that gets rendered by pug

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+function getRoot(req, res) {
+  res.render("generic", {
+    title: "Verein"
+  });
+}
+
+module.exports = function(db) {
+  var router = express.Router();
+
+  router.get('/', getRoot);
+
+  return router;
+}
