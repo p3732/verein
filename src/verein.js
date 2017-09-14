197ba3f9206e0e6f5978f4aa17569e03e302175f
p3732 <p3732@users.noreply.github.com>
2017-09-14 10:03:33 +0200

    verein - added execution mode notification

--- src/verein.js
+++ src/verein.js
@@ -2,16 +2,14 @@ const log  = require("./logging.js")("verein");
 
 /*
  * Starting function. Startup order is:
- * A-load configuration
+ * A-config
  *   load recursively from ./config directory
- * B-connect and init db with sequelize
- *   1- establish connection
- *   2- initialization
- *   3- schemes sync
- *   4-[opt if creating a new db] db initialization with default values
- * C-set up routing with express
- *   set routes recursively from ./api directory
- * D-bind to localhost and start
+ * B-database
+ *   connect and init db with sequelize
+ * C-router
+ *   set up routing with express and set pug to be used for rendering pages
+ * D-server
+ *   bind to localhost and start
  */
 function start() {
   var config = require("./config.js");
@@ -23,11 +21,20 @@ function start() {
   config.load("./config")
   //.then(log.debug(JSON.stringify(config)))
   .then(db.init(config.db))
-  .then(router.init(router, db))
+  .then(router.init(router, db, config.mode))
   .then(server.start(config.server, router))
+  .then(modeWarning(config.mode))
   .catch(function (err) {
     log.error("Server crashed. Error was: ", err);
   });
+}
+
+function modeWarning(mode) {
+  if (mode !== "normal") {
+    log("################################### !!! Running in " + mode + " mode !!! ###################################");
+  } else {
+    log("Running in normal mode.");
+  }
 }
 
 module.exports = start;
