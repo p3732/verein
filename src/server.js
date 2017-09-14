763670634dce36df0012d703440a2cd4e5fd780b
p3732 <p3732@users.noreply.github.com>
2017-09-14 09:59:46 +0200

    server - fix error when already running, noe exits normally

--- src/server.js
+++ src/server.js
@@ -15,30 +15,27 @@ this.start = function(server_config, router) {
     host = (host == "::") ? "localhost" : host;
     log("listening at " + host + ':' + port);
   });
-  server.on("error", onError);
-}
-
-/** Event listener for HTTP server "error" event. */
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  var bind = (typeof port === "string" ? "Pipe " : "Port ") + port;
-
-  // handle specific listen errors
-  switch (error.code) {
-    case "EACCES":
-      log.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      log.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
+  server.on("error", function onError(error) {
+    if (error.syscall !== "listen") {
       throw error;
-  }
+    }
+
+    var bind = (typeof port === "string" ? "Pipe " : "Port ") + port;
+
+    // handle specific listen errors
+    switch (error.code) {
+      case "EACCES":
+        log.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        log.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  });
 }
 
 module.exports = this;
